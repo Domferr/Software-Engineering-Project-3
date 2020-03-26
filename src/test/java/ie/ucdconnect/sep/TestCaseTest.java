d0f2f493ac84f1c215b7e7dd481dd15873d79ecb
Gnxela <qawswedfrgtt@gmail.com>
2020-03-26 13:42:25 +0000

    Moved tests to test directory.

---
+++ src/test/java/ie/ucdconnect/sep/TestCaseTest.java
@@ -0,0 +1,18 @@
+package ie.ucdconnect.sep;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class TestCaseTest {
+
+	@BeforeAll
+	public void setup() {
+		System.out.println("Setup");
+	}
+	
+	@Test
+	public void testSpecialFocus() {
+		System.out.println("Case");
+	}
+	
+}
