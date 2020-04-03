a83560a9dc3c006a6941b8877a2cd9c1539788f3
Domenico <ferraro.domenico125@gmail.com>
2020-04-03 09:11:33 +0200

    Generating and saving solution for each test set

--- src/main/java/ie/ucdconnect/sep/SolutionGenerator.java
+++ src/main/java/ie/ucdconnect/sep/SolutionGenerator.java
@@ -1,14 +1,19 @@
 package ie.ucdconnect.sep;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+/** This class reads the testcases and generates a random solution.
+ *  For each project it prints the assigned student. */
 public class SolutionGenerator {
 
 	private static final int CASE_NUM = 500;
@@ -17,21 +22,48 @@ public class SolutionGenerator {
 	public static void main(String[] args) throws IOException {
 		config = Config.getInstance();
 		List<StaffMember> staffMembers = StaffMember.fromCSV(readFile(config.getStaffMembersFile().toPath()));
-		List<Project> projects = Project.fromCSV(readFile(getProjectFile().toPath()), staffMembers);
-		Map<String, Project> projectsMap = projects.stream().collect(Collectors.toMap(Project::getTitle, Function.identity()));
-		List<Student> students = Student.fromCSV(readFile(getStudentsFile().toPath()), projectsMap);
-		Solution solution = Solution.createRandom(projects, students);
+		int[] testSetsSize = config.getTestSetsStudentsSize();
+		for (int i = 0; i < testSetsSize.length; i++) {
+			Solution solution = generateSolution(testSetsSize[i], staffMembers);
 
-		System.out.println(solution);
+			saveSolution(solution, testSetsSize[i]);
+			System.out.println("Generated solution for "+testSetsSize[i]+" students.");
+		}
 	}
 
-	private static File getProjectFile() {
-		String fileName = "projectsFor" + CASE_NUM + "Students.csv";
+	/** Write the given list into specified file.  */
+	private static void saveSolution(Solution solution, int size) {
+		String dirName = config.getTestcaseDirName();
+		File testCaseDir = new File(dirName);
+		if (!testCaseDir.exists())
+			testCaseDir.mkdir();
+
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(dirName+"solutionFor"+size+"students.txt"));
+			String fileContent = solution.toString();
+			writer.write(fileContent);
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/** Given a testset size, reads the right projects and students files and returns a random solution */
+	private static Solution generateSolution(int testSetSize, List<StaffMember> staffMembers) throws IOException {
+		List<Project> projects = Project.fromCSV(readFile(getProjectFile(testSetSize).toPath()), staffMembers);
+		Map<String, Project> projectsMap = projects.stream().collect(Collectors.toMap(Project::getTitle, Function.identity()));
+		List<Student> students = Student.fromCSV(readFile(getStudentsFile(testSetSize).toPath()), projectsMap);
+
+		return Solution.createRandom(projects, students);
+	}
+
+	private static File getProjectFile(int testSetSize) {
+		String fileName = "projectsFor" + testSetSize + "Students.csv";
 		return new File(config.getTestcaseDirName() + fileName);
 	}
 
-	private static File getStudentsFile() {
-		String fileName = "students" + CASE_NUM + ".csv";
+	private static File getStudentsFile(int testSetSize) {
+		String fileName = "students" + testSetSize + ".csv";
 		return new File(config.getTestcaseDirName() + fileName);
 	}
 
