8158b718ea883eb359a67bd5ca8e0f82dfcd8b60
Domenico <ferraro.domenico125@gmail.com>
2020-04-11 12:36:32 +0200

    Simulated Annealing algorithm

--- src/main/java/ie/ucdconnect/sep/SolutionGenerator.java
+++ src/main/java/ie/ucdconnect/sep/SolutionGenerator.java
@@ -22,6 +22,10 @@ public class SolutionGenerator implements Default{
 	private static final int GENERATION_SIZE = 250;
 	// The number of "bad" solutions that will be removed at the end of each generation.
 	private static final int GENERATION_CULL = 150;
+	// The number of iterations that SA algorithm will do before lowering the temperature
+	private static final double MINIMA_NUM_ITERATIONS = 100;
+	// How much the starting temperature should be higher than the maxEnergyDelta
+	private static final double MAX_TEMPERATURE_INCREASE = 10;
 
 	private static Random random;
 
@@ -38,7 +42,8 @@ public class SolutionGenerator implements Default{
 		List<Student> students = Utils.readStudents(Utils.generateProjectsMap(projects), test_size);
 
 		//Run genetic algorithm
-		Solution solution = runGeneticAlgorithm(projects, students);
+		//Solution solution = runGeneticAlgorithm(projects, students);
+		Solution solution = runSimulatedAnnealingAlgorithm(projects, students);
 		System.out.println("Final energy: " + solution.getEnergy() + ". Final fitness: " + solution.getFitness() + ".");
 
 		//Save generated solution into resources dir
@@ -63,15 +68,10 @@ public class SolutionGenerator implements Default{
 	}
 
 	/**
-	 * Runs a genetic algorithm and returns the best result.
+	 * Runs the genetic algorithm and returns the best result.
 	 */
 	private static Solution runGeneticAlgorithm(List<Project> projects, List<Student> students) {
-		List<Solution> solutions = new ArrayList<>();
-
-		//Create the initial search space
-		for (int i = 0; i < GENERATION_SIZE; i++) {
-			solutions.add(createOneRandomSolution(projects, students));
-		}
+		List<Solution> solutions = getRandomSolutionList(projects, students, GENERATION_SIZE);
 
 		for (int i = 0; i < NUM_GENERATIONS; i++) {
 			System.out.println("Running generation: " + i);
@@ -81,6 +81,56 @@ public class SolutionGenerator implements Default{
 		}
 
 		return solutions.get(0);
+	}
+
+	/**
+	 * Runs the simulated algorithm and returns the best result.
+	 */
+	private static Solution runSimulatedAnnealingAlgorithm(List<Project> projects, List<Student> students) {
+		//Calculate max energy delta in 100 random solutions
+		double maxEnergyDelta = calculateMaxEnergyDelta(getRandomSolutionList(projects, students, 100));
+		//Set the initial temperature as higher than MaxEnergyDelta
+		double temperature = maxEnergyDelta + (MAX_TEMPERATURE_INCREASE * random.nextDouble());
+		//Generate one random currentSolution
+		Solution currentSolution = createOneRandomSolution(projects, students);
+		Solution bestSolution = currentSolution;
+		while(temperature > 0) {
+			//Find a local minima
+			for (int i = 0; i < MINIMA_NUM_ITERATIONS; i++) {
+				Solution mutatedSolution = mutate(currentSolution, projects);
+				double deltaEnergy = Math.abs(mutatedSolution.getEnergy() - currentSolution.getEnergy());
+				//Accepting if the changes have better energy
+				if (mutatedSolution.getEnergy() < currentSolution.getEnergy())
+					currentSolution = mutatedSolution;
+				//Otherwise accept with Boltzmann probability
+				else if (random.nextDouble() <= Math.exp(-deltaEnergy/temperature))
+					currentSolution = mutatedSolution;
+			}
+			//Remember the global minima
+			if (currentSolution.getEnergy() < bestSolution.getEnergy())
+				bestSolution = currentSolution;
+			//Lower the temperature based on how much high it is
+			temperature -= temperature*random.nextDouble(); //The higher it is the more it is lowered
+		}
+
+		return bestSolution;
+	}
+
+	/** Returns the max energy delta calculated from the given list of solutions */
+	private static double calculateMaxEnergyDelta(List<Solution> solutions) {
+		double maxDelta = 0;
+
+		for (Solution thisSolution :solutions) {
+			for (Solution otherSolution :solutions) {
+				if (!thisSolution.equals(otherSolution)) {
+					double delta = Math.abs(thisSolution.getEnergy() - otherSolution.getEnergy());
+					if (delta > maxDelta)
+						maxDelta = delta;
+				}
+			}
+		}
+
+		return maxDelta;
 	}
 
 	private static List<Solution> mutate(List<Solution> solutions, List<Project> projects) {
@@ -109,6 +159,16 @@ public class SolutionGenerator implements Default{
 		}
 
 		return Solution.SolutionFactory.createAndEvaluate(mapBuilder.build(), GPA_IMPORTANCE);
+	}
+
+	/** Returns a list of random solutions */
+	public static List<Solution> getRandomSolutionList(List<Project> projects, List<Student> students, int howMany) {
+		List<Solution> solutions = new ArrayList<>();
+		//Create a list of solutions and find the max energy delta
+		for (int i = 0; i < howMany; i++) {
+			solutions.add(createOneRandomSolution(projects, students));
+		}
+		return solutions;
 	}
 
 	/**
