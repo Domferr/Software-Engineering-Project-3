1b327697dfc9a2a4dba845193799352d8a4a0d25
Martynas Jagutis <martynasjagutis99@gmail.com>
2020-04-18 13:32:25 +0100

    added Simulated Annealing unit test

--- src/test/java/ie/ucdconnect/sep/generators/SimulatedAnnealingTest.java
+++ src/test/java/ie/ucdconnect/sep/generators/SimulatedAnnealingTest.java
@@ -1,17 +1,42 @@
 package ie.ucdconnect.sep.generators;
 
+import com.google.common.collect.ImmutableMultimap;
+import ie.ucdconnect.sep.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimulatedAnnealingTest {
 
+    private Solution randSolution;
+    private Solution annealingSolution;
+    private List<Project> projects;
+    private List<Student> students;
+
     @BeforeEach
-    void setUp() {
+    void setUp() throws IOException {
+        int[] testSetsStudentsSize = Config.getInstance().getTestSetsStudentsSize();
+        int test_size = testSetsStudentsSize[1];
+        List<StaffMember> staffMembers = Utils.readStaffMembers();
+        projects = Utils.readProjects(staffMembers, test_size);
+        students = Utils.readStudents(Utils.generateProjectsMap(projects), test_size);
     }
 
     @Test
     void generate() {
+        randSolution = new RandomGeneration().generate(projects, students, 1.0);
+        annealingSolution = new SimulatedAnnealing().generate(projects, students, 1.0);
+        assertTrue(annealingSolution.getEnergy() < randSolution.getEnergy());
+        assertNotEquals(randSolution, annealingSolution);
+        randSolution = new RandomGeneration().generate(projects, students, 0);
+        annealingSolution = new SimulatedAnnealing().generate(projects, students, 0);
+        assertTrue(annealingSolution.getEnergy() < randSolution.getEnergy());
     }
+
+
 }
