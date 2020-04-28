56342b4baa1dbaf1875b55a3c4f4eedd026f088d
Domenico <ferraro.domenico125@gmail.com>
2020-04-28 12:01:00 +0200

    Added css and static function to change root of the scene

--- src/main/java/ie/ucdconnect/sep/MainApplication.java
+++ src/main/java/ie/ucdconnect/sep/MainApplication.java
@@ -7,16 +7,29 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class MainApplication extends Application {
 
+    private static Scene scene;
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+    public void start(Stage primaryStage) throws Exception {
+        scene = new Scene(loadFXML("/sample"));
+        scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 400, 350));
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+    public static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
 
     public static void main(String[] args) {
         launch(args);
