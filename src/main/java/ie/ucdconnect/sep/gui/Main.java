c2a9f5e6c95670f69a0a6ae83a241656bda4c49d
Gnxela <qawswedfrgtt@gmail.com>
2020-05-02 15:38:08 +0100

    Fixed Java FX bug unique to Java 11.

---
+++ src/main/java/ie/ucdconnect/sep/gui/Main.java
@@ -0,0 +1,11 @@
+package ie.ucdconnect.sep.gui;
+
+public class Main {
+
+	/**
+	 * Fixes a bug in Java 11 where JavaFX components are not properly loaded.
+	 */
+	public static void main(String[] args) {
+		MainApplication.main(args);
+	}
+}
