c1ac40dcb98b62ca0f9481bf8cb19aead41b4a89
Martynas Jagutis <martynasjagutis99@gmail.com>
2020-05-14 20:21:10 +0100

    fixed bug by running evaluation of solution before displaying on table

--- src/main/java/ie/ucdconnect/sep/gui/SolutionTable.java
+++ src/main/java/ie/ucdconnect/sep/gui/SolutionTable.java
@@ -33,32 +33,6 @@ public class SolutionTable {
     private void setUp() {
         studentColumn.setCellValueFactory((p) -> new SimpleStringProperty(p.getValue().getValue().getName()));
         assignedProjectColumn.setCellValueFactory((p) -> new SimpleStringProperty(p.getValue().getKey().getTitle()));
-
-        studentColumn.setCellFactory(new Callback<TableColumn<Map.Entry<Project, Student>, String>, TableCell<Map.Entry<Project, Student>, String>>() {
-            @Override
-            public TableCell<Map.Entry<Project, Student>, String> call(TableColumn<Map.Entry<Project, Student>, String> param) {
-                return new TableCell<Map.Entry<Project, Student>, String>(){
-                    Student student;
-                    @Override
-                    public void updateItem(String item, boolean empty){
-                        super.updateItem(item, empty);
-                        student = null;
-                        this.setTextFill(Color.BLACK);
-
-                        if(getIndex() < getTableView().getItems().size() && getIndex() != -1)
-                             student = getTableView().getItems().get(getIndex()).getValue();
-                        if(student!=null && !student.isGotPreference() && !empty ) {
-                            this.setTextFill(Color.RED);
-                        }
-
-
-
-                        setText(item);
-                    }
-                };
-            }
-
-        });
         solutionTableView.getColumns().setAll(studentColumn, assignedProjectColumn);
         //Sort by students
         solutionTableView.getSortOrder().add(studentColumn);
@@ -66,11 +40,40 @@ public class SolutionTable {
     }
 
     public void showSolution(Solution solution) {
+
         if (solution != null) {
+            solution.evaluate();
             //This list is needed because the columns should be sorted by the user
             List<Map.Entry<Project, Student>> entriesList = new ArrayList<>(solution.getEntries());
             solutionTableView.setItems(FXCollections.observableList(entriesList));
 
+            for(Map.Entry<Project, Student> entry : entriesList){
+                System.out.println(entry.getValue().getName()+ " " + entry.getValue().isGotPreference() + "\n");
+            }
+            
+            studentColumn.setCellFactory(new Callback<TableColumn<Map.Entry<Project, Student>, String>, TableCell<Map.Entry<Project, Student>, String>>() {
+                @Override
+                public TableCell<Map.Entry<Project, Student>, String> call(TableColumn<Map.Entry<Project, Student>, String> param) {
+                    return new TableCell<Map.Entry<Project, Student>, String>(){
+                        Student student;
+                        @Override
+                        public void updateItem(String item, boolean empty){
+                            super.updateItem(item, empty);
+                            student = null;
+                            this.setTextFill(Color.BLACK);
+
+                            if(getIndex() < getTableView().getItems().size() && getIndex() != -1)
+                                student = getTableView().getItems().get(getIndex()).getValue();
+                            if(student!=null && !student.isGotPreference() && !empty ) {
+                                this.setTextFill(Color.RED);
+                            }
+
+                            setText(item);
+                        }
+                    };
+                }
+
+            });
             showSaveBtn();
         }
     }
