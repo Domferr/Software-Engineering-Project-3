9b44929f633d3499ded9b9eea42cd6c2ad7d87e2
Domenico <ferraro.domenico125@gmail.com>
2020-05-15 19:06:00 +0200

    Chart bugfix

--- src/main/java/ie/ucdconnect/sep/gui/AlgorithmStats.java
+++ src/main/java/ie/ucdconnect/sep/gui/AlgorithmStats.java
@@ -8,7 +8,13 @@ import javafx.scene.Group;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+
+import java.awt.*;
 
 /** Class that manages the chart that shows the algorithm performance */
 public class AlgorithmStats {
@@ -23,6 +29,8 @@ public class AlgorithmStats {
     public AlgorithmStats(BarChart<String, Number> energyAndFitnessChart, BarChart<String, Number> reportStudentsPreference) {
         this.energyAndFitness = energyAndFitnessChart;
         this.reportStudentsPreference = reportStudentsPreference;
+        this.reportStudentsPreference.setAnimated(false);
+        this.energyAndFitness.setAnimated(false);
         this.seriesEnergy = new XYChart.Series<>();
         this.seriesFitness = new XYChart.Series<>();
         this.seriresPreferences = new XYChart.Series<>();
@@ -60,11 +68,12 @@ public class AlgorithmStats {
 
     private XYChart.Data<String, Number> createXYData(String str, double value) {
         StackPane stackPane = new StackPane();
-        Group main = new Group(new Label(((int)value)+""));
-        StackPane.setAlignment(main, Pos.CENTER);
-        StackPane.setMargin(main, new Insets(0, 0, 16, 0));
-        stackPane.getChildren().add(main);
-
+        String label = ((int) value) + "";
+        Group group = new Group(new Label(label));
+        StackPane.setAlignment(group, Pos.CENTER);
+        StackPane.setMargin(group, new Insets(0, 0, 16, 0));
+        stackPane.getChildren().add(group);
+        
         XYChart.Data<String, Number> data =  new XYChart.Data<>(str, value);
         data.setNode(stackPane);
         return data;
